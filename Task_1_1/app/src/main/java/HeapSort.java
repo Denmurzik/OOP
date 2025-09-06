6fe89eca7102fcc89ddf1caa5134fad0fa489258
Denis Muruev <d.muruev@g.nsu.ru>
2025-09-06 22:16:03 +0700

    vb

--- Task_1_1/app/src/main/java/HeapSort.java
+++ Task_1_1/app/src/main/java/HeapSort.java
@@ -2,7 +2,7 @@
 
 public class HeapSort {
 
-    public void sort(int[] arr) {
+    public static void sort(int[] arr) {
         if (arr == null || arr.length <= 1) {
             return;
         }
@@ -20,7 +20,7 @@ public class HeapSort {
         }
     }
 
-    public void heapify(int[] arr, int n, int i) {
+    private static void heapify(int[] arr, int n, int i) {
         int largest = i;
         int left = 2 * i + 1;
         int right = 2 * i + 2;
@@ -41,7 +41,7 @@ public class HeapSort {
     }
 
 
-    public void swap(int[] arr, int i, int j) {
+    private static void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
