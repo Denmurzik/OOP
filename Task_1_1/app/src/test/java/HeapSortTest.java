aa1326772fde84a18845d57be4828d36ddadeb24
Denis Muruev <d.muruev@g.nsu.ru>
2025-09-06 22:18:59 +0700

    vb

--- Task_1_1/app/src/test/java/HeapSortTest.java
+++ Task_1_1/app/src/test/java/HeapSortTest.java
@@ -1,23 +1,13 @@
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class HeapSortTest {
-    
-    private HeapSort sorter;
-    
-    @BeforeEach
-    void setUp() {
-        sorter = new HeapSort();
-    }
-
 
     @Test
     void SingleElementArray() {
         int[] testArray = {1};
         int[] answerArray = {1};
-        sorter.sort(testArray);
+        HeapSort.sort(testArray);
         assertArrayEquals(answerArray, testArray);
     }
 
@@ -25,7 +15,7 @@ class HeapSortTest {
     void ReverseSortedArray() {
         int[] testArray = {985345, 76, 9, 8, 4, 5, 2, 1};
         int[] answerArray = {1, 2, 4, 5, 8, 9, 76, 985345,};
-        sorter.sort(testArray);
+        HeapSort.sort(testArray);
         assertArrayEquals(answerArray, testArray);
     }
 
@@ -35,7 +25,7 @@ class HeapSortTest {
         int[] testArray = {5, 2, 8, 1, 9};
         int[] answerArray = {1, 2, 5, 8, 9};
         
-        sorter.sort(testArray);
+        HeapSort.sort(testArray);
         
         assertArrayEquals(answerArray, testArray);
     }
@@ -46,7 +36,7 @@ class HeapSortTest {
         int[] testArray = {};
         int[] answerArray = {};
         
-        sorter.sort(testArray);
+        HeapSort.sort(testArray);
 
         assertArrayEquals(answerArray, testArray);
     }
@@ -57,7 +47,7 @@ class HeapSortTest {
         int[] testArray = {1, 2, 3, 4, 5};
         int[] answerArray = {1, 2, 3, 4, 5};
 
-        sorter.sort(testArray);
+        HeapSort.sort(testArray);
 
         assertArrayEquals(answerArray, testArray);
     }
@@ -68,7 +58,7 @@ class HeapSortTest {
         int[] testArray = {23, 5, 2, 8, 2, 5, 1, 23};
         int[] answerArray = {1, 2, 2, 5, 5, 8, 23, 23};
 
-        sorter.sort(testArray);
+        HeapSort.sort(testArray);
 
         assertArrayEquals(answerArray, testArray);
     }
