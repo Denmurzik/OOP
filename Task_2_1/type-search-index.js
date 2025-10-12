d88dba914e702a3cfebee66f07adfd96edf0f242
Denmurzik <Denmurzik@users.noreply.github.com>
2025-10-12 06:32:39 +0000

    Deploying to gh-pages from @ Denmurzik/OOP@3c2686575756ffbd3777229a75476d842036ecb6 🚀

---
+++ Task_2_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"org","l":"AdjacencyListGraph"},{"p":"org","l":"AdjacencyMatrixGraph"},{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org","l":"Graph"},{"p":"org","l":"GraphAlgorithms"},{"p":"org","l":"IncidenceMatrixGraph"},{"p":"org","l":"Main"}];updateSearchResults();
