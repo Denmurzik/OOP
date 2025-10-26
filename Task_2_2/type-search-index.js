73e843c3d75748b8a19d61232ac03d54d4dee808
Denmurzik <Denmurzik@users.noreply.github.com>
2025-10-26 14:54:26 +0000

    Deploying to gh-pages from @ Denmurzik/OOP@ad95184bc0b96dfb9bf7f3e0353c2a219ec27d12 🚀

--- Task_2_2/type-search-index.js
+++ Task_2_2/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org","l":"HashTable"},{"p":"org","l":"HashTable.Node"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org","l":"HashTable"},{"p":"org","l":"Node"}];updateSearchResults();
