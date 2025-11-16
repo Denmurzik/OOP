37e5ee24b98129ccdce32fb6eb54b462f2030f90
Denmurzik <Denmurzik@users.noreply.github.com>
2025-11-16 09:25:11 +0000

    Deploying to gh-pages from @ Denmurzik/OOP@7359d92aee0b261a5416680ff40bb57cd1af7b82 🚀

---
+++ Task_4_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"Grade"},{"p":"org.example","l":"GradeBook"},{"p":"org.example","l":"GradeType"}];updateSearchResults();
