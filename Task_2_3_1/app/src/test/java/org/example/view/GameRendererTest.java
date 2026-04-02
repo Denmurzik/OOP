2474c304a3a832978e1b2f7c792c52a1bea67fb4
Denis Muruev <d.muruev@g.nsu.ru>
2026-04-02 09:31:41 +0700

    а

--- Task_2_3_1/app/src/test/java/org/example/view/GameRendererTest.java
+++ Task_2_3_1/app/src/test/java/org/example/view/GameRendererTest.java
@@ -1,17 +1,8 @@
 package org.example.view;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import javafx.application.Platform;
-import javafx.scene.canvas.Canvas;
-import org.example.model.GameConfig;
-import org.example.model.GameField;
-import org.example.model.GameModel;
-import org.example.model.WinCondition;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
-import java.util.Random;
 
 class GameRendererTest {
 
